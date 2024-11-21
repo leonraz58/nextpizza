cd9df71c190e26c59e6ca6483fda45ad2a3ac1d5
leonraz58 <leonraz58@gmail.com>
2024-11-21 21:34:04 +0300

    add main section container

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Container} from "@/components/shared";
+import {Container, Filters} from "@/components/shared";
 import {Title} from "@/components/shared/title";
 import {TopBar} from "@/components/shared/top-bar";
 
@@ -9,6 +9,18 @@ export default function Home() {
                 <Title text="Все пиццы" size="lg" className="font-extrabold"/>
             </Container>
             <TopBar/>
+            <Container className="pb-14">
+                <div className="flex gap-[60px]">
+                    <div className="w-[250px]">
+                        <Filters />
+                    </div>
+                    <div className="flex-1">
+                        <div className="flex flex-col gap-16">
+                            Список товаров
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </>
     );
 }
