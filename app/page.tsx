ef0e0bf023e5b6eb778e15fc579aae340fec8b21
leonraz58 <leonraz58@gmail.com>
2024-11-21 20:43:49 +0300

    add Categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Container} from "@/components/shared";
+import {Categories, Container} from "@/components/shared";
 import {Title} from "@/components/shared/title";
 
 export default function Home() {
@@ -6,6 +6,8 @@ export default function Home() {
         <>
             <Container className="mt-10">
                 <Title text="Все пиццы" size="lg" className="font-extrabold"/>
+
+                <Categories/>
             </Container>
         </>
     );
