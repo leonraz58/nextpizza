411d8e54384b37baf395be74e6049568dd12a5ba
leonraz58 <leonraz58@gmail.com>
2024-11-26 02:28:36 +0300

    add SearchInput

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import {cn} from "@/lib/utils";
 import Image from 'next/image';
 import {Button} from "@/components/ui/button";
 import {ArrowRight, ShoppingCart, User} from "lucide-react";
+import {SearchInput} from "./search-input";
+import React from "react";
 
 interface Props {
     className?: string;
@@ -23,6 +25,13 @@ export const Header: React.FC<Props> = ({className}) => {
                         </div>
                     </div>
                 </Link>
+
+
+                <div className="mx-10 flex-1">
+                    <SearchInput/>
+                </div>
+
+
                 <div className="flex items-center gap-1">
                     <Button variant="outline">
                         <User size={16}/>
@@ -32,12 +41,13 @@ export const Header: React.FC<Props> = ({className}) => {
                     <div>
                         <Button className="group relative">
                             <b>520 ₽</b>
-                            <span className="h-full w-[1px] bg-white/30 mx-3" />
+                            <span className="h-full w-[1px] bg-white/30 mx-3"/>
                             <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-                                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2}/>
                                 <b>3</b>
                             </div>
-                            <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+                            <ArrowRight
+                                className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"/>
                         </Button>
                     </div>
                 </div>
