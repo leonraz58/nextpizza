aee0824dfe7e7fbdfc2cb4cb45254eb09b98ffe4
leonraz58 <leonraz58@gmail.com>
2024-11-27 02:09:43 +0300

    use ingredients api in CheckboxFiltersGroup

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,15 +1,23 @@
+'use client'
+
 import React from 'react';
 
 import { Title } from './title';
 import {FilterCheckbox} from "./filter-checkbox";
 import {Input, RangeSlider} from "@/components/ui";
 import { CheckboxFiltersGroup } from './checkbox-filters-group';
+import {useIngredients} from "@/hooks/use-ingredients";
 
 interface Props {
     className?: string;
 }
 
 export const Filters: React.FC<Props> = ({ className }) => {
+
+    const { ingredients } = useIngredients();
+
+    const items = ingredients.map((item) => ({ value: String(item.id), text: item.name }));
+
     return (
         <div className={className}>
             <Title text="Фильтрация" size="sm" className="mb-5 font-bold"/>
@@ -31,26 +39,8 @@ export const Filters: React.FC<Props> = ({ className }) => {
             <CheckboxFiltersGroup title={"Ингредиенты"}
                                   className={'mt-5'}
                                   limit={6}
-                                  items={[
-                                      { text: 'Сырный соус', value: '1' },
-                                      { text: 'Моцарелла', value: '2' },
-                                      { text: 'Чеснок', value: '3' },
-                                      { text: 'Соленые огурчики', value: '4' },
-                                      { text: 'Красный лук', value: '5' },
-                                      { text: 'Томаты', value: '6' },
-                                      { text: 'Тонкое', value: '7' },
-                                      { text: 'Традиционное', value: '8' },
-                                  ]}
-                                  defaultItems={[
-                                      { text: 'Сырный соус', value: '1' },
-                                      { text: 'Моцарелла', value: '2' },
-                                      { text: 'Чеснок', value: '3' },
-                                      { text: 'Соленые огурчики', value: '4' },
-                                      { text: 'Красный лук', value: '5' },
-                                      { text: 'Томаты', value: '6' },
-                                      { text: 'Тонкое', value: '7' },
-                                      { text: 'Традиционное', value: '8' },
-                                  ]}
+                                  defaultItems={items.slice(0, 6)}
+                                  items={items}
             />
         </div>
     );
---
+++ services/ingredients.ts
@@ -0,0 +1,7 @@
+import {axiosInstance} from "@/services/instance";
+import {Ingredient} from "@prisma/client";
+import {ApiRoutes} from "@/services/constants";
+
+export const getAll = async (): Promise<Ingredient[]> => {
+    return (await axiosInstance.get<Ingredient[]>(ApiRoutes.INGREDIENTS)).data;
+};
