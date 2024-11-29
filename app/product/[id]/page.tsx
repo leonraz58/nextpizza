84b976ec73018bf48d5ed307aecbdb8c8920795e
leonraz58 <leonraz58@gmail.com>
2024-11-30 02:51:14 +0300

    fix product page flex

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,6 @@
 import {prisma} from "@/prisma/prisma-client";
 import {notFound} from "next/navigation";
-import {Container, ProductImage} from "@/components/shared";
+import {Container, ProductImage, Title} from "@/components/shared";
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
 
@@ -12,7 +12,16 @@ export default async function ProductPage({ params: { id } }: { params: { id: st
 
     return (
         <Container className={"flex flex-col my-10"}>
-            <ProductImage imageUrl={product.imageUrl} size={40}/>
+            <div className="flex flex-1">
+                <ProductImage imageUrl={product.imageUrl} size={40}/>
+
+                <div className={"w-[490px] bg-[#F7F6F5] p-7"}>
+                    <Title text={product.name} size="md" className={"font-extrabold mb-1"}/>
+
+                    <p className={"text-gray-400"}>Lorem ipsum dolor sit amet</p>
+                </div>
+            </div>
+
         </Container>
     )
 }
