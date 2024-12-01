f8b202282e22a76122faf22fc797553e5f8d8e16
leonraz58 <leonraz58@gmail.com>
2024-12-01 04:20:52 +0300

    add isPizzaForm check

--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -5,17 +5,19 @@ import { useRouter } from 'next/navigation';
 import {Dialog} from "@/components/ui";
 import {DialogContent} from "@/components/ui/dialog";
 import {cn} from "@/lib/utils";
-import {Product} from "@prisma/client";
 import {ChooseProductForm} from "@/components/shared/choose-product-form";
+import {ProductWithRelations} from "@/@types/prisma";
 
 
 interface Props {
-  product: Product;
+  product: ProductWithRelations;
   className?: string;
 }
 
 export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
   const router = useRouter();
+  const isPizzaForm = Boolean(product.items[0].pizzaType)
+
 
   return (
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
@@ -24,7 +26,8 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
           'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden',
           className,
         )}>
-          <ChooseProductForm imageUrl={product.imageUrl} name={product.name} ingredients={[]} />
+        {isPizzaForm ? ('PizzaForm') : <ChooseProductForm imageUrl={product.imageUrl} name={product.name} ingredients={[]} />}
+
       </DialogContent>
     </Dialog>
   );
