ab53967129c353e190065ea098f671b1b80fc764
leonraz58 <leonraz58@gmail.com>
2024-12-01 03:27:57 +0300

    add ChooseProductModal component

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,45 +1,31 @@
 import {prisma} from "@/prisma/prisma-client";
 import {notFound} from "next/navigation";
-import {Container, GroupVariants, ProductImage, Title} from "@/components/shared";
+import {ChooseProductModal} from "@/components/shared";
 
 export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
 
-    const product = await prisma.product.findFirst({ where:{ id: Number(id) } })
+    const product = await prisma.product.findFirst({
+        where: { id: Number(id) },
+        include: {
+            ingredients: true,
+            category: {
+                include: {
+                    products: {
+                        include: {
+                            items: true,
+                        },
+                    },
+                },
+            },
+            items: true,
+        },
+    });
 
     if (!product) {
-        return notFound()
+        return notFound();
     }
 
     return (
-        <Container className={"flex flex-col my-10"}>
-            <div className="flex flex-1">
-                <ProductImage imageUrl={product.imageUrl} size={40}/>
-
-                <div className={"w-[490px] bg-[#F7F6F5] p-7"}>
-                    <Title text={product.name} size="md" className={"font-extrabold mb-1"}/>
-
-                    <p className={"text-gray-400"}>Lorem ipsum dolor sit amet</p>
-
-                    <GroupVariants
-                        selectedValue={'2'}
-                        items={[
-                            {
-                                name: 'Маленькая',
-                                value: '1'
-                            },
-                            {
-                                name: 'Средняя',
-                                value: '2'
-                            },
-                            {
-                                name: 'Большая',
-                                value: '3',
-                                disabled: true,
-                            },
-                        ]}/>
-                </div>
-            </div>
-
-        </Container>
+        <ChooseProductModal product={product}/>
     )
 }
