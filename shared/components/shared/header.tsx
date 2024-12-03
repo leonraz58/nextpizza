39089b44b53b3dacfa2b10c24cc19aeed803bc7d
leonraz58 <leonraz58@gmail.com>
2024-12-03 22:48:33 +0300

    refactor - CartButton

--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 import {cn} from "@/shared/lib/utils";
 import Image from 'next/image';
 import {Button} from "@/shared/components/ui/button";
-import {ArrowRight, ShoppingCart, User} from "lucide-react";
+import {User} from "lucide-react";
 import {SearchInput} from "./search-input";
 import React from "react";
+import {CartButton} from "@/shared/components/shared/cart-button";
 
 interface Props {
     className?: string;
@@ -31,25 +32,13 @@ export const Header: React.FC<Props> = ({className}) => {
                     <SearchInput/>
                 </div>
 
-
                 <div className="flex items-center gap-1">
                     <Button variant="outline">
                         <User size={16}/>
                         Войти
                     </Button>
 
-                    <div>
-                        <Button className="group relative">
-                            <b>520 ₽</b>
-                            <span className="h-full w-[1px] bg-white/30 mx-3"/>
-                            <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-                                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2}/>
-                                <b>3</b>
-                            </div>
-                            <ArrowRight
-                                className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"/>
-                        </Button>
-                    </div>
+                    <CartButton/>
                 </div>
             </Container>
         </header>
