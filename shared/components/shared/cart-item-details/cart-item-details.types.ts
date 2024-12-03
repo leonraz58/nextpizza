c899dac95987ae9abef3870f7c6a3ea5ad2ed93d
leonraz58 <leonraz58@gmail.com>
2024-12-03 23:59:44 +0300

    add CartItemDetails components

---
+++ shared/components/shared/cart-item-details/cart-item-details.types.ts
@@ -0,0 +1,7 @@
+export interface CartItemProps {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
---
+++ shared/components/shared/cart-item-details/cart-item-info.tsx
@@ -0,0 +1,32 @@
+import {Ingredient} from "@prisma/client";
+import {mapPizzaType, PizzaSize, PizzaType} from "@/shared/constants/pizza";
+
+
+interface Props {
+    name: string;
+    pizzaSize?: PizzaSize
+    type?: PizzaType;
+    ingredients?: Ingredient[];
+}
+
+export const CartItemInfo: React.FC<Props> = ({ name, pizzaSize, type, ingredients }) => {
+    const details = [];
+
+    if (pizzaSize && type) {
+        const typeName = mapPizzaType[type];
+        details.push(`${typeName} ${pizzaSize} см`);
+    }
+
+    if (ingredients) {
+        details.push(...ingredients.map((ingredient) => ingredient.name));
+    }
+
+    return (
+        <div>
+            <div className="flex items-center justify-between">
+                <h2 className="text-lg font-bold flex-1 leading-6">{name}</h2>
+            </div>
+            {details.length > 0 && <p className="text-xs text-gray-400">{details.join(', ')}</p>}
+        </div>
+    );
+};
