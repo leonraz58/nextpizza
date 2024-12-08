ec54d784c033f1323c9d32711ec4bdbf449308e8
leonraz58 <leonraz58@gmail.com>
2024-12-09 01:10:03 +0300

    add react-hook-form, update form logic in FormInput

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -5,14 +5,34 @@ import {
     Container,
     Title,
     CheckoutCart,
-    CheckoutPersonalForm
+    CheckoutPersonalForm,
+    CheckoutAddressForm
 } from "@/shared/components/shared";
 import {useCart} from "@/shared/hooks";
-import {CheckoutAddressForm} from "@/shared/components/shared/checkout/checkout-address-form";
+import {checkoutFormSchema, CheckoutFormValues} from "@/shared/constants";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {FormProvider, useForm} from "react-hook-form";
+import toast from "react-hot-toast";
 
 export default function CheckoutPage() {
 
-    const { totalAmount, updateItemQuantity, items, removeCartItem, loading } = useCart();
+    const {totalAmount, updateItemQuantity, items, removeCartItem, loading} = useCart();
+
+    const form = useForm<CheckoutFormValues>({
+        resolver: zodResolver(checkoutFormSchema),
+        defaultValues: {
+            email: '',
+            firstName: '',
+            lastName: '',
+            phone: '',
+            address: '',
+            comment: '',
+        },
+    });
+
+    const onSubmit = async (data: CheckoutFormValues) => {
+        console.log('aaa')
+    };
 
     const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
         const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
@@ -22,17 +42,22 @@ export default function CheckoutPage() {
     return (
         <Container className="mt-10">
             <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]"/>
-            <div className="flex gap-10">
-                <div className={'flex flex-col gap-10 flex-1 mb-20'}>
-                    <CheckoutCart items={items} onClickCountButton={onClickCountButton} removeCartItem={removeCartItem}/>
-                    <CheckoutPersonalForm/>
-                    <CheckoutAddressForm/>
-                </div>
+            <FormProvider {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className="flex gap-10">
+                        <div className={'flex flex-col gap-10 flex-1 mb-20'}>
+                            <CheckoutCart items={items} onClickCountButton={onClickCountButton}
+                                          removeCartItem={removeCartItem}/>
+                            <CheckoutPersonalForm/>
+                            <CheckoutAddressForm/>
+                        </div>
 
-                <div className="w-[450px]">
-                    <CheckoutSidebar totalAmount={totalAmount} />
-                </div>
-            </div>
+                        <div className="w-[450px]">
+                            <CheckoutSidebar totalAmount={totalAmount}/>
+                        </div>
+                    </div>
+                </form>
+            </FormProvider>
         </Container>
     );
 }
--- shared/components/shared/form/form-input.tsx
+++ shared/components/shared/form/form-input.tsx
@@ -1,32 +1,49 @@
 'use client';
 
+import { useFormContext } from 'react-hook-form';
 import { Input } from '../../ui/input';
 import { ClearButton } from '../clear-button';
+import { ErrorText } from '../error-text';
 import { RequiredSymbol } from '../required-symbol';
 import React from "react";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-  name: string;
-  label?: string;
-  required?: boolean;
-  className?: string;
+    name: string;
+    label?: string;
+    required?: boolean;
+    className?: string;
 }
 
 export const FormInput: React.FC<Props> = ({ className, name, label, required, ...props }) => {
+    const {
+        register,
+        formState: { errors },
+        watch,
+        setValue,
+    } = useFormContext();
 
-  return (
-    <div className={className}>
-      {label && (
-        <p className="font-medium mb-2">
-          {label} {required && <RequiredSymbol />}
-        </p>
-      )}
+    const value = watch(name);
+    const errorText = errors[name]?.message as string;
 
-      <div className="relative">
-        <Input className="h-12 text-md"  {...props} />
+    const onClickClear = () => {
+        setValue(name, '', { shouldValidate: true });
+    };
 
-        {<ClearButton />}
-      </div>
-    </div>
-  );
+    return (
+        <div className={className}>
+            {label && (
+                <p className="font-medium mb-2">
+                    {label} {required && <RequiredSymbol />}
+                </p>
+            )}
+
+            <div className="relative">
+                <Input className="h-12 text-md" {...register(name)} {...props} />
+
+                {value && <ClearButton onClick={onClickClear} />}
+            </div>
+
+            {errorText && <ErrorText text={errorText} className="mt-2" />}
+        </div>
+    );
 };
