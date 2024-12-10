eae54e15ce818c67a22a5b24d375c20758360f53
leonraz58 <leonraz58@gmail.com>
2024-12-10 23:55:38 +0300

    fix useQueryFilters params rewrite

--- shared/hooks/use-query-filters.ts
+++ shared/hooks/use-query-filters.ts
@@ -1,21 +1,25 @@
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 import qs from "qs";
 import {Filters} from "@/shared/hooks/use-filters";
 import {useRouter} from "next/navigation";
 
 export const useQueryFilters = (filters: Filters) => {
+    const isMounted = useRef(false);
     const router = useRouter()
 
-    useEffect(()=>{
-        const params = {
-            ...filters.prices,
-            pizzaTypes: Array.from(filters.pizzaTypes),
-            sizes: Array.from(filters.sizes),
-            ingredients: Array.from(filters.selectedIngredients)
+    useEffect(() => {
+        if (isMounted.current) {
+            const params = {
+                ...filters.prices,
+                pizzaTypes: Array.from(filters.pizzaTypes),
+                sizes: Array.from(filters.sizes),
+                ingredients: Array.from(filters.selectedIngredients)
+            }
+
+            const query = qs.stringify(params, {arrayFormat: 'comma'})
+
+            router.push(`?${query}`, {scroll: false})
         }
-
-        const query = qs.stringify(params, {arrayFormat: 'comma'})
-
-        router.push(`?${query}`, {scroll: false})
+        isMounted.current = true;
     }, [filters]);
 }
